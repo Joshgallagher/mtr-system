419d8fca8ffedd5b1cafd34edd6ac903abe9abff
Joshua Gallagher <joshuagallagher@Joshuas-MacBook-Pro.local>
2017-11-27 17:39:00 +0000

    More refactoring

--- src/Model/AbstractNode.java
+++ src/Model/AbstractNode.java
@@ -7,67 +7,67 @@ import java.util.List;
 
 public abstract class AbstractNode {
 
-    private final HashSet<AbstractNode> CONNECTED_NODES = new HashSet<AbstractNode>();
-    
-    /**
-     * 
-     * @param node
-     */
-    public void addConnectedNode(AbstractNode node) {
-        CONNECTED_NODES.add(node);
-    }
-    
-    /**
-     * 
-     * @return
-     */
-    public HashSet<AbstractNode> getConnectedNodes() {
-        return CONNECTED_NODES;
-    }
-    
-    /**
-     * 
-     * @param endNode
-     * @return
-     */
-    public List<AbstractNode> bfsFromHere(AbstractNode endNode) {
-    	
-        List<AbstractNode> pathList = new LinkedList<>();
-        LinkedList<AbstractNode> queue = new LinkedList<>();
-        
-        HashSet<AbstractNode> visited = new HashSet<AbstractNode>();
-        HashMap<AbstractNode, AbstractNode> parents = new HashMap<>();
+	private final HashSet<AbstractNode> connectedNodes = new HashSet<AbstractNode>();
 
-        queue.add(this);
-        visited.add(this);
+	/**
+	 * 
+	 * @param node
+	 */
+	public void addConnectedNode(AbstractNode node) {
+		connectedNodes.add(node);
+	}
 
-        while(!queue.isEmpty()) {
-            AbstractNode currentNode = queue.remove();
+	/**
+	 * 
+	 * @return
+	 */
+	public HashSet<AbstractNode> getConnectedNodes() {
+		return connectedNodes;
+	}
 
-            if(currentNode == endNode) {
-                break;
-            } else {
-                    for (AbstractNode node : currentNode.getConnectedNodes()) {
-                        if (!visited.contains(node)) {
-                            queue.add(node);
-                            parents.put(node, currentNode);
-                        }
-                    }
-                visited.add(currentNode);
-            }
-        }
-        
-        if (parents.get(endNode) == null) {
-        		return null;
-        }
-        
-        AbstractNode curNode = endNode;
-        
-        while (curNode != null) {
-        		pathList.add(0, curNode);
-        		curNode = parents.get(curNode);
-        }
-        
-        return pathList;
-    }
+	/**
+	 * 
+	 * @param endNode
+	 * @return
+	 */
+	public List<AbstractNode> bfsFromHere(AbstractNode endNode) {
+
+		List<AbstractNode> pathList = new LinkedList<>();
+		LinkedList<AbstractNode> queue = new LinkedList<>();
+
+		HashSet<AbstractNode> visited = new HashSet<AbstractNode>();
+		HashMap<AbstractNode, AbstractNode> parents = new HashMap<>();
+
+		queue.add(this);
+		visited.add(this);
+
+		while (!queue.isEmpty()) {
+			AbstractNode currentNode = queue.remove();
+
+			if (currentNode == endNode) {
+				break;
+			} else {
+				for (AbstractNode node : currentNode.getConnectedNodes()) {
+					if (!visited.contains(node)) {
+						queue.add(node);
+						parents.put(node, currentNode);
+					}
+				}
+				visited.add(currentNode);
+			}
+		}
+
+		if (parents.get(endNode) == null) {
+			return null;
+		}
+
+		AbstractNode curNode = endNode;
+
+		while (curNode != null) {
+			pathList.add(0, curNode);
+			curNode = parents.get(curNode);
+		}
+
+		return pathList;
+	}
 }
