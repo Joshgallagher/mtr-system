c853c72d82caad6836f7baef35f3a3899c98b78e
Joshua Gallagher <joshuagallagher@Joshuas-MacBook-Pro.local>
2017-11-27 14:21:03 +0000

    Implemented BFS. Currently a working implementation, however, it does not follow the printing standards set by Sylvia.

--- src/Model/AbstractNode.java
+++ src/Model/AbstractNode.java
@@ -1,7 +1,9 @@
 package model;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 
 public abstract class AbstractNode {
 
@@ -28,9 +30,13 @@ public abstract class AbstractNode {
      * @param endNode
      * @return
      */
-    public String bfsFromHere(AbstractNode endNode) {
-        LinkedList<AbstractNode> queue = new LinkedList<AbstractNode>();
+    public List<AbstractNode> bfsFromHere(AbstractNode endNode) {
+    	
+        List<AbstractNode> pathList = new LinkedList<>();
+        LinkedList<AbstractNode> queue = new LinkedList<>();
+        
         HashSet<AbstractNode> visited = new HashSet<AbstractNode>();
+        HashMap<AbstractNode, AbstractNode> parents = new HashMap<>();
 
         queue.add(this);
         visited.add(this);
@@ -39,20 +45,33 @@ public abstract class AbstractNode {
             AbstractNode currentNode = queue.remove();
 
             if(currentNode == endNode) {
-                return "PATH FOUND - SOMEONE IMPLEMENT THE PATH TRACE. IM UNDER THE NODE CLASS";
+                break;
             } else {
                 if(currentNode.getConnectedNodes().isEmpty()) {
-                    return "PATH NOT FOUND";
+                    break;
                 } else {
                     for (AbstractNode node : currentNode.getConnectedNodes()) {
                         if (!visited.contains(node)) {
                             queue.add(node);
+                            parents.put(node, currentNode);
                         }
                     }
                 }
                 visited.add(currentNode);
             }
         }
-        return "PATH NOT FOUND";
+        
+        if (parents.get(endNode) == null) {
+        		return null;
+        }
+        
+        AbstractNode curNode = endNode;
+        
+        while (curNode != null) {
+        		pathList.add(0, curNode);
+        		curNode = parents.get(curNode);
+        }
+        
+        return pathList;
     }
 }
