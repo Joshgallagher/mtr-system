0e3ca3815333208179c4fb221e79341c512381ae
Joshua Gallagher <joshgallagher24@gmail.com>
2018-02-02 17:47:40 +0000

    Changed folder name - Resources -> resources. Updated reference to data file in core.

--- src/core/MTRSystem.java
+++ src/core/MTRSystem.java
@@ -9,11 +9,11 @@ import view.TUIView;
 /**
  * The <code>MTRSystem<code> is the class which contains the entry point to this
  * program.
- * 
+ *
  * @author Joshua Gallagher
  * @author Thomas Geraghty
  * @author Dimitar Stoynev
- * 
+ *
  * @version 15/10/2017
  */
 public class MTRSystem {
@@ -21,14 +21,14 @@ public class MTRSystem {
 	/**
 	 * MTRSystem is the core class. This class brings all the components together,
 	 * from within the system.
-	 * 
+	 *
 	 * It passes the the data files location to the <code>Reader</code>, which is
 	 * passed with an instance of <code>MTR</code> to the <code>Interpreter</code>
 	 * to create a model of the underground system.
-	 * 
+	 *
 	 * Then, the text based view is set up to present the output of the program to a
 	 * user.
-	 * 
+	 *
 	 * @param args
 	 */
 	public static void main(String[] args) {
